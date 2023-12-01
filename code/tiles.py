a037eed57de6ae79bd794995505095f690c97bfd
Jannis <mprogrammierer@gmail.com>
2023-12-01 16:48:59 +0100

    comment out code

--- code/tiles.py
+++ code/tiles.py
@@ -3,9 +3,12 @@ import pygame
 class Tile(pygame.sprite.Sprite):
 	def __init__(self,pos,size):
 		super().__init__()
+		
+		# tile setup
 		self.image = pygame.Surface((size,size))
 		self.image.fill('grey')
 		self.rect = self.image.get_rect(topleft = pos)
 	
 	def update(self,x_shift):
+		# move the tile 
 		self.rect.x += x_shift
