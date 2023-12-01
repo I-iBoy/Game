b1f6f7ffcaedf6fa50d4b43cd0b8c616e18b60cd
Jannis <mprogrammierer@gmail.com>
2023-12-01 16:25:30 +0100

    Adapt the speed settings

--- code/level.py
+++ code/level.py
@@ -13,6 +13,7 @@ class Level():
         self.display_surface = surface 
         self.setup_level(level_data)
         self.world_shift = 0
+        self.world_shift_setup = 25
         self.current_x = 0
         
         # player
@@ -39,15 +40,15 @@ class Level():
         player_x = player.rect.centerx
         direction_x = player.direction.x
         
-        if player_x < screen_width / 4 and direction_x < 0:
-            self.world_shift = 8
+        if player_x < (screen_width / 3) and direction_x < 0:
+            self.world_shift = self.world_shift_setup
             player.speed = 0
-        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
-            self.world_shift = -8
+        elif player_x > screen_width - (screen_width / 3) and direction_x > 0:
+            self.world_shift = self.world_shift_setup * -1
             player.speed = 0
         else:
             self.world_shift = 0
-            player.speed = 4
+            player.speed = player.real_speed
     
     def horizontal_movement_collosion(self):
         player = self.player.sprite
--- code/player.py
+++ code/player.py
@@ -11,7 +11,8 @@ class Player(pygame.sprite.Sprite):
         
         # player movement 
         self.direction = pygame.math.Vector2(0, 0)
-        self.speed = 4
+        self.speed_setup = 3
+        self.speed = self.speed_setup
         self.gravity = 0.8
         self.jump_speed = -16
         
