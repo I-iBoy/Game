bf2b0a3bca1dc2819538622353394416b534ef33
Jannis <mprogrammierer@gmail.com>
2023-12-15 18:55:07 +0100

    control optimized

--- code/player.py
+++ code/player.py
@@ -3,8 +3,7 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
-        
-        # general setup 
+# control optimized        # general setup 
         self.image = pygame.Surface((size, size))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
@@ -27,7 +26,7 @@ class Player(pygame.sprite.Sprite):
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.direction.x = self.speed * -1
         
-        if keys[pygame.K_SPACE] and self.on_ground or keys[pygame.K_w] and self.on_ground:
+        if keys[pygame.K_SPACE] and self.on_ground or keys[pygame.K_w] and self.on_ground or keys[pygame.K_UP] and self.on_ground:
             self.direction.y = self.jump_speed
         
         else:
