fe2e3654b2ba9dba3de35da27aa2500efac50200
Jannis <mprogrammierer@gmail.com>
2023-12-18 11:42:40 +0100

    Test map enlarged

--- code/setting.py
+++ code/setting.py
@@ -1,17 +1,17 @@
 
 # test level map
 level_map = [
-'                            ', # 0
-'                            ', # 1
-'        C              C    ', # 2
-' XX    XXX            XX    ', # 3
-' XX P          C         C  ', # 4
-' XXXX         XX         XX ', # 5
-' XXXX     C XX     C        ', # 6
-' XX    X  XXXX    XX  XX    ', # 7
-'    C  X  XXXX    XX  XXX   ', # 8
-'    XXXX  XXXXXX  XX  XXXX  ', # 9
-'XXXXXXXX  XXXXXX  XX  XXXX  ' # 10
+'                                                                                    ', # 0
+'                                                                                    ', # 1
+'        C              C            C              C            C              C    ', # 2
+' XX    XXX            XX     XX    XXX            XX     XX    XXX            XX    ', # 3
+' XX P          C         C   XX            C         C   XX            C         C  ', # 4
+' XXXX         XX         XX  XXXX         XX         XX  XXXX         XX         XX ', # 5
+' XXXX     C XX     C         XXXX     C XX     C         XXXX     C XX     C        ', # 6
+' XX    X  XXXX    XX  XX     XX    X  XXXX    XX  XX     XX    X  XXXX    XX  XX    ', # 7
+'    C  X  XXXX    XX  XXX       C  X  XXXX    XX  XXX       C  X  XXXX    XX  XXX   ', # 8
+'    XXXX  XXXXXX  XX  XXXX      XXXX  XXXXXX  XX  XXXX      XXXX  XXXXXX  XX  XXXX  ', # 9
+'XXXXXXXX  XXXXXX  XX  XXXX  XXXXXXXX  XXXXXX  XX  XXXX  XXXXXXXX  XXXXXX  XX  XXXX  ' # 10
 ]
 
 
