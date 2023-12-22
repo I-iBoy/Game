83ae80b2fb42d35e289c8fde02feb582dabb64f8
Jannis <mprogrammierer@gmail.com>
2023-12-22 19:15:35 +0100

    begin to set the animation

--- code/tiles.py
+++ code/tiles.py
@@ -10,6 +10,7 @@ class Tile(pygame.sprite.Sprite):
 		self.image.fill('grey')
         # pygame.image.load()
 		self.rect = self.image.get_rect(topleft = (x, y))
+        
 	
 	def update(self,x_shift):
 		# move the tile 
@@ -24,6 +25,27 @@ class StaticTile(Tile):
         self.rect.x += x_shift
 
 class AnimatedTile(Tile):
+    def __init__(self, size, x, y, frames, path=None, value = 0):
+        super().__init__(size, x, y)
+        self.animation_speed = 0.15
+        self.frames = frames
+        self.frame_index = 0
+        self.image = self.frames[self.frame_index]
+        self.value = value
+    
+    def animate(self):
+        self.frame_index += self.animation_speed
+        if self.frame_index >= self.frames:
+            self.frame_index = 0
+        
+        self.image = self.frames[int(self.frame_index)]
+    
+    def update(self, x_shift):
+        self.animate()											
+        # #NOTE - if the coin has multiple images / frames -> for the animation 
+        self.rect.x += x_shift
+
+class AnimatedTile1(Tile):
     def __init__(self, size, x, y, path=None, value = 0):
         super().__init__(size, x, y)
         self.frames = import_folder(path)
@@ -43,7 +65,7 @@ class AnimatedTile(Tile):
         # #NOTE - if the coin has multiple images / frames -> for the animation 
         self.rect.x += x_shift
 
-class Coin(StaticTile):
+class Coin(AnimatedTile):
     def __init__(self, x, y, size, value=None):
         super().__init__(size, x, y, pygame.image.load('.\images\coins/0.png').convert_alpha())
         self.image = pygame.transform.scale(self.image, (size*0.6, size*0.6))
